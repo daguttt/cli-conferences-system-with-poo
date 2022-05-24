7cbe7c000a52ee10af74a404913e98613d636db0
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-24 12:42:38 -0500

    style: Format index.ts and tsconfign.json

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,72 @@
-
-import { Menu } from './Menu'
-
+import { Menu } from "./Menu";
 
 /**
  * puedes borrar este ejemplo para tu entrega final
  */
-const ejemplo = (texto:string, numero:number):void => {
+const ejemplo = (texto: string, numero: number): void => {
   console.log(`
   
   Haz ingresado el texto: ${texto}  y el número ${numero}
   
   `);
-  
-}
+};
 
 (async () => {
-  const menu = new Menu()
-    while (menu.isActive()) {
+  const menu = new Menu();
+  while (menu.isActive()) {
+    menu.printMenu();
+    let key = await menu.getInt("seleccione un número de la lista:");
 
-      menu.printMenu()
-      let key = await menu.getInt('seleccione un número de la lista:')
-    
-      switch (key) {
-        case 0:
-          console.log('0: ',key);
-          menu.close()
-          process.exit()
+    switch (key) {
+      case 0:
+        console.log("0: ", key);
+        menu.close();
+        process.exit();
 
-          break;
-        
-        case 1:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-        
-        case 2:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-        
-        case 3:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-      
-        case 4:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-      
-        case 5:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-      
-        case 6:
-          console.log('Falta por implementar la opción: : ',key);
-          break;
-        
-        case 7:
-          /**Buenes borrar este caso para tu entrega final */
-          
-          const texto = await menu.getString('ingresa un texto cualquiera, luego preciona enter')
-          const numero = await  menu.getInt('ingresa un número, luego preciona enter')
-          ejemplo(texto,numero)
-          break;
-      
-        default:
-          console.log('Debe elegir una opción valida');
-          //menu.close()
-          break;
-      }
+        break;
+
+      case 1:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 2:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 3:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 4:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 5:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 6:
+        console.log("Falta por implementar la opción: : ", key);
+        break;
+
+      case 7:
+        /**Buenes borrar este caso para tu entrega final */
+
+        const texto = await menu.getString(
+          "ingresa un texto cualquiera, luego preciona enter"
+        );
+        const numero = await menu.getInt(
+          "ingresa un número, luego preciona enter"
+        );
+        ejemplo(texto, numero);
+        break;
+
+      default:
+        console.log("Debe elegir una opción valida");
+        //menu.close()
+        break;
     }
-    
-  console.log('Adios');
-    
-  })()
+  }
 
-
+  console.log("Adios");
+})();
