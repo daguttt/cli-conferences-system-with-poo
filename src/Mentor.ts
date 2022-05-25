6ef4a20ec25f86c20cc7b066cdcb6e48b57c004a
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-25 10:11:31 -0500

    feat: Implement "verifyMentorAvailability"

--- src/Mentor.ts
+++ src/Mentor.ts
@@ -1,9 +1,19 @@
+import { Conference } from "./Conference";
 import { Person } from "./shared/person";
-
 export class Mentor extends Person {
-  public conferences: number[];
+  public conferences: Conference[];
   constructor(name: string, email: string, password: string) {
     super(name, email, password);
     this.conferences = [];
+  }
+  public static verifyMentorAvailability(
+    mentor: Mentor,
+    startingDateEvent: Date
+  ): boolean {
+    if (mentor.conferences.length)
+      return mentor.conferences.every((conference) => {
+        conference.endingDate.getTime() <= startingDateEvent.getTime();
+      });
+    return true;
   }
 }
