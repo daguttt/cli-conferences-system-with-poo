73e473032729ea31a9b5aa8de4d134f25e1350de
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-25 10:12:25 -0500

    feat: Implement method "authMentor"

--- src/Mentor.ts
+++ src/Mentor.ts
@@ -1,5 +1,7 @@
 import { Conference } from "./Conference";
 import { Person } from "./shared/person";
+import { Response } from "./Response";
+import Store from "./Store";
 export class Mentor extends Person {
   public conferences: Conference[];
   constructor(name: string, email: string, password: string) {
@@ -15,5 +17,24 @@ export class Mentor extends Person {
         conference.endingDate.getTime() <= startingDateEvent.getTime();
       });
     return true;
+  }
+  public static authMentor(email: string, password: string): Response {
+    const response = new Response();
+    const mentorExists: boolean = Store.mentorExists(email);
+    if (!mentorExists) {
+      response.error = true;
+      response.message =
+        "Según las credenciales ingresadas el mentor no está registrado en el sistema";
+      return response;
+    }
+    const mentor = Store.getMentorThatAlreadyExists(email);
+    if (!(mentor.password === password)) {
+      response.error = true;
+      response.message = "Contraseña incorrecta";
+      return response;
+    }
+    response.error = false;
+    response.message = "Mentor autenticado con éxito";
+    return response;
   }
 }
