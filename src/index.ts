b676ffebd4db667707acfe1445cddddd890613ed
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-25 11:00:04 -0500

    refactor: Extract menu options functionality to its onw class

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,13 @@
 import { Mentor } from "./Mentor";
-import { Menu } from "./Menu";
+import { Menu } from "./menu/Menu";
+import { MenuOptions } from "./menu/MenuOptions";
 import Store from "./Store";
 
 // -*************************************************************************-
 
-// STUDENTS
-const addStudent = (name: string, email: string, password: string): void => {
-  // ! I'm not using the 'error'
-  const { message } = Store.storeStudent(name, email, password);
-  console.log("\n");
-  console.log(message);
-};
-
-const showStudents = (): void => {
-  if (Store.students.length)
-    Store.students?.forEach((student) => {
-      console.log(`
-            Nombre: ${student.name}
-            Email: ${student.email}
-            `);
-    });
-  else console.log("No hay estudiantes registrados");
-};
-
 // -*************************************************************************-
 
 // MENTORS
-const addMentor = (name: string, email: string, password: string): void => {
-  // ! I'm not using the 'error'
-  const { message } = Store.storeMentor(name, email, password);
-  console.log("\n");
-  console.log(message);
-};
-
-const showMentors = (): void => {
-  if (Store.mentors.length)
-    Store.mentors?.forEach((mentor) => {
-      console.log(`
-            Nombre: ${mentor.name}
-            Email: ${mentor.email}
-            `);
-    });
-  else console.log("No hay mentores registrados");
-};
 
 (async () => {
   const menu = new Menu();
@@ -57,49 +22,27 @@ const showMentors = (): void => {
         process.exit();
 
       case 1:
-        addMentor(
-          await menu.getString("Introduce el nombre: "),
-          await menu.getString("Introduce el email: "),
-          await menu.getString("Introduce una contraseña: ")
-        );
+        await MenuOptions.addMentor();
         break;
 
       case 2:
-        addStudent(
-          await menu.getString("Introduce el nombre: "),
-          await menu.getString("Introduce el email: "),
-          await menu.getString("Introduce una contraseña: ")
-        );
+        await MenuOptions.addStudent();
         break;
 
       case 3:
-        const mentorEmail = await menu.getString("Introduce tu correo:");
-        const mentorPassword = await menu.getString("Introduce tu contraseña:");
-        const { error: errorOfAuth, message: authMessage } = Mentor.authMentor(
-          mentorEmail,
-          mentorPassword
-        );
-        console.log(authMessage);
-        if (errorOfAuth) break;
-        const { message: storeMessage } = Store.addConference(
-          mentorEmail,
-          await menu.getString("Introduce el título de tu conferencia:"),
-          new Date(await menu.getString("Introduce la fecha de inicio:")),
-          new Date(await menu.getString("Introduce la fecha de fin:"))
-        );
-        console.log(storeMessage);
+        await MenuOptions.addConference();
         break;
 
       case 4:
         console.log("\n");
         console.log("\n");
-        showStudents();
+        MenuOptions.showStudents();
         break;
 
       case 5:
         console.log("\n");
         console.log("\n");
-        showMentors();
+        MenuOptions.showMentors();
 
         break;
 
---
+++ src/menu/MenuOptions.ts
@@ -0,0 +1,77 @@
+import { Menu } from "./Menu";
+import Store from "../Store";
+import { Mentor } from "../Mentor";
+
+export class MenuOptions extends Menu {
+  constructor() {
+    super();
+  }
+  public static async addMentor(): Promise<void> {
+    const name = await this.prototype.getString("Introduce el nombre: ");
+    const email = await this.prototype.getString("Introduce el email: ");
+    const password = await this.prototype.getString(
+      "Introduce una contraseña: "
+    );
+    const { message } = Store.storeMentor(name, email, password);
+    console.log("\n");
+    console.log(message);
+  }
+  public static async addStudent(): Promise<void> {
+    const name = await this.prototype.getString("Introduce el nombre: ");
+    const email = await this.prototype.getString("Introduce el email: ");
+    const password = await this.prototype.getString(
+      "Introduce una contraseña: "
+    );
+    const { message } = Store.storeStudent(name, email, password);
+    console.log("\n");
+    console.log(message);
+  }
+  public static async addConference(): Promise<void> {
+    const mentorEmail = await this.prototype.getString("Introduce tu correo:");
+    const mentorPassword = await this.prototype.getString(
+      "Introduce tu contraseña:"
+    );
+    const { error: errorOfAuth, message: authMessage } = Mentor.authMentor(
+      mentorEmail,
+      mentorPassword
+    );
+    console.log(authMessage);
+    if (errorOfAuth) return;
+    const { message: storeMessage } = Store.addConference(
+      mentorEmail,
+      await this.prototype.getString("Introduce el título de tu conferencia:"),
+      new Date(await this.prototype.getString("Introduce la fecha de inicio:")),
+      new Date(await this.prototype.getString("Introduce la fecha de fin:"))
+    );
+    console.log(storeMessage);
+  }
+  public static showStudents(): void {
+    if (Store.students.length)
+      Store.students?.forEach((student) => {
+        console.log(`
+            Nombre: ${student.name}
+            Email: ${student.email}
+            `);
+      });
+    else console.log("No hay estudiantes registrados");
+  }
+  public static showMentors(): void {
+    if (Store.mentors.length)
+      Store.mentors?.forEach((mentor) => {
+        console.log(`
+            Nombre: ${mentor.name}
+            Email: ${mentor.email}
+            `);
+      });
+    else console.log("No hay mentores registrados");
+  }
+  public static showConferences(): void {
+    if (Store.conferences.length)
+      Store.conferences?.forEach(({ name, startingDate, endingDate }) => {
+        console.log(`Título evento: ${name}`);
+        console.log(`Fecha de inicio ---------- Fecha de finalizacion`);
+        console.log(`${startingDate}  ---------- ${endingDate}`);
+      });
+    else console.log("No hay conferencias");
+  }
+}
